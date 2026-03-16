9d218e0dc1a4c46f71a0b8f7decf694c80063583
phoenixixixi <mhammmaddustin@gmail.com>
2026-03-16 23:09:27 +0700

    fix a bug on deleteById

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -142,8 +142,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteById(id: string) {
-  throw new Error('Failed to Delete Invoice')
-
   try {
     await sql`delete from invoices where id = ${id}`
   } catch (err) {
